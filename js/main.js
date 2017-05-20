0c5f21d24d9bc1a92871fc6a81a19dd7c90b0e39
Sarat Maharjan <Sarat Maharjan>
2017-05-20 15:22:53 +0200

    basic js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 
+
+var days = [1, 2, 3, 4, 5];
+
+var dailyItems = {
+	"one": ["buy something", "buy nothing"],
+	"two": ["buy something", "buy nothing"],
+	"three": ["buy something", "buy nothing"],
+	"four": ["buy something", "buy nothing"],
+	"five": ["buy something", "buy nothing"]
+}
+
+var dailyDOM = document.getElementById("daily");
+
+dailyDOM.innerHTML = "<ul>";
+
+for (var i = 0; i < dailyItems.one.length; i++) {
+
+	dailyDOM.innerHTML += "<li>" + dailyItems.one[i] + "</li>";
+
+}
+
+dailyDOM.innerHTML += "</ul>";
